2970429a01bcc28db80fc054a0195d9d420c83f9
Kittipod Lambangchang <tonkittipod@gmail.com>
2023-07-15 18:29:41 +0700

    feat: add ch3_5.py

--- stack/ch3_5.py
+++ stack/ch3_5.py
@@ -16,3 +16,38 @@ class Stack:
 
   def size(self):
     return len(self.stack)
+  
+def soi_ton(max_car, cars, command, car):
+  stack = Stack()
+  
+  for i in cars:
+    if i != '0':
+      stack.push(int(i)) 
+    
+  if command == 'arrive':
+    if stack.size() < max_car and car not in stack.stack:
+      stack.push(car)
+      print(f'car {car} arrive! : Add Car {car}')
+    elif car in stack.stack:
+      print(f'car {car} already in soi')
+    else:
+      print(f'car {car} cannot arrive : Soi Full')
+  
+  elif command == 'depart':
+    if stack.is_empty():
+      print(f'car {car} cannot depart : Soi Empty')
+    elif car in stack.stack:
+      stack.stack.remove(car)
+      print(f'car {car} depart ! : Car {car} was remove')
+    elif car not in stack.stack:
+      print(f'car {car} cannot depart : Dont Have Car {car}')
+      
+    
+  print(stack.stack)
+  
+if __name__ == '__main__':
+  print('******** Parking Lot ********')
+  
+  max_car, cars, command, car = [i for i in input("Enter max of car,car in soi,operation : ").split()]
+  
+  soi_ton(int(max_car), cars.split(','), command, int(car))
