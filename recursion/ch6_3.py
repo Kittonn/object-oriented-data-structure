eda77d61fbdcae6929b40f8c08cbd64235765c2b
Kittipod Lambangchang <tonkittipod@gmail.com>
2023-08-21 21:38:58 +0700

    feat(recursion): add chapter 6 item 3

--- recursion/ch6_3.py
+++ recursion/ch6_3.py
@@ -1,13 +1,29 @@
-def jump(n):
-  diff = [7,5,1]
-  for i in diff:
-    
+def jump(start, end, count):
+  count += 1
+  if start % 14 == 0 and start != 0:
+    return
+  if start == end:
+    global max_way
+    max_way += 1
+    global distance
+    distance.append(count)
+    return
+  if start > end:
+    return
+  else:
+    jump(start + 1, end, count)
+    jump(start + 5, end, count)
+    jump(start + 7, end, count)
+
 
 if __name__ == "__main__":
-  count_way = 0
-  count_dist = 0
   n = int(input("Input number : "))
-  if (n % 14 == 0):
+  max_way = 0
+  distance = []
+  c = 0
+  jump(0, n, c)
+  if (max_way == 0):
     print("Mission Failed")
-  else:  
-    print(jump(n))
+  else:
+    print(f"Minimum Distance is {min(distance) - 1}")
+    print(f"Maximum Way is {max_way}")
