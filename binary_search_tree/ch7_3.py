188e334088b2fd84aacb867430ae7a4bc7d0f66f
Kittonn <tonkittipod@gmail.com>
2023-09-23 17:23:13 +0700

    feat(binary tree): update chapter 7 item 3

--- binary_search_tree/ch7_3.py
+++ binary_search_tree/ch7_3.py
@@ -1,35 +1,30 @@
 def find_max_path(idx, path):
-  global paths 
+    global paths
 
-  if idx > len(inputs) - 1:
-    if path not in paths:
-      paths.append(path)
-    return
+    if idx > len(inputs) - 1:
+        if path not in paths:
+            paths.append(path)
+        return
 
-  
-  path_left = path.copy()
-  path_right = path.copy()
-  path_left.append(inputs[idx])
-  path_right.append(inputs[idx])
-  find_max_path(idx * 2 +1, path_left)
-  find_max_path(idx * 2 +2, path_right)
+    path_left = path.copy()
+    path_right = path.copy()
+    path_left.append(inputs[idx])
+    path_right.append(inputs[idx])
+    find_max_path(idx * 2 + 1, path_left)
+    find_max_path(idx * 2 + 2, path_right)
 
 
-if __name__ == '__main__':
-  inputs = [int(i) for i in input('Enter tree: ').split()]
-  paths = []
+if __name__ == "__main__":
+    inputs = [int(i) for i in input("Enter tree: ").split()]
+    paths = []
 
-  find_max_path(0, [])
+    find_max_path(0, [])
 
-  max_path = []
-  max_sum = float('-inf')
-  for path in paths:
-    if sum(path) > max_sum:
-      max_sum = sum(path)
-      max_path = path
-  
-  print(f'Maximum path: {max_path}')
-    
-  
+    max_path = []
+    max_sum = float("-inf")
+    for path in paths:
+        if sum(path) > max_sum:
+            max_sum = sum(path)
+            max_path = path
 
-  
+    print(f"Maximum path: {max_path}")
