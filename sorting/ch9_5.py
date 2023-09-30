127c8aa99cb0c3c74077c08d33881a771baae231
Kittonn <tonkittipod@gmail.com>
2023-09-30 17:16:42 +0700

    feat(sorting): update chapter 9 item 5

--- sorting/ch9_5.py
+++ sorting/ch9_5.py
@@ -1,58 +1,43 @@
-def partition(arr, low, high, ascending=True):
-    pivot = arr[high]
-    i = low - 1
-    for j in range(low, high):
-        if (arr[j] <= pivot) if ascending else (arr[j] >= pivot):
-            i += 1
-            arr[i], arr[j] = arr[j], arr[i]
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
+def find_subsets(subset, arr):
+    if sum(subset) == target:
+        subsets.append(subset)
 
-def quick_sort(arr, low, high, ascending=True):
-    if low < high:
-        pi = partition(arr, low, high, ascending)
-        quick_sort(arr, low, pi - 1, ascending)
-        quick_sort(arr, pi + 1, high, ascending)
+    for i in range(len(arr)):
+        find_subsets(subset + [arr[i]], arr[i + 1 :])
 
-def find_subsets(temp_arr, data):
-    if sum(temp_arr) == s:
-        ans_arr.append(temp_arr)
-        
-    
-    for i in range(len(data)):
-        find_subsets(temp_arr + [data[i]], data[i+1:])
+    return subsets
 
-    return ans_arr
-    
 
-if __name__ == '__main__':
-    s, inp = input('Enter Input : ').split('/')
+if __name__ == "__main__":
+    target, inp = input("Enter Input : ").split("/")
+    inp = list(map(int, inp.split()))
+    target = int(target)
 
-    s = int(s)
-    inp = [int(i) for i in inp.split()]
+    subsets = []
 
-    ans_arr = []
     find_subsets([], inp)
 
-    if len(ans_arr) == 0:
-        print('No Subset')
-    else:
-        for i in ans_arr:
-            quick_sort(i, 0, len(i) - 1, ascending=True)
+    if len(subsets) == 0:
+        print("No Subset")
+        exit(0)
 
-        for l in range(len(ans_arr) - 1, 0, -1):
-            for i in range(l):
-                if len(ans_arr[i]) > len(ans_arr[i + 1]):
-                    ans_arr[i], ans_arr[i + 1] = ans_arr[i + 1], ans_arr[i]
-                elif len(ans_arr[i]) == len(ans_arr[i + 1]):
-                    for j in range(len(ans_arr[i])):
-                        if ans_arr[i][j] > ans_arr[i + 1][j]:
-                            ans_arr[i], ans_arr[i + 1] = ans_arr[i + 1], ans_arr[i]
-                            break
-                        elif ans_arr[i][j] < ans_arr[i + 1][j]:
-                            break
-                        
-        for i in ans_arr:
-            print(i)
+    for subset in subsets:
+        for i in range(len(subset)):
+            for j in range(len(subset) - i - 1):
+                if subset[j] > subset[j + 1]:
+                    subset[j], subset[j + 1] = subset[j + 1], subset[j]
 
+    for i in range(len(subsets)):
+        for j in range(len(subsets) - i - 1):
+            if len(subsets[j]) > len(subsets[j + 1]):
+                subsets[j], subsets[j + 1] = subsets[j + 1], subsets[j]
+            elif len(subsets[j]) == len(subsets[j + 1]):
+                for k in range(len(subsets[j])):
+                    if subsets[j][k] > subsets[j + 1][k]:
+                        subsets[j], subsets[j + 1] = subsets[j + 1], subsets[j]
+                        break
+                    else:
+                        break
 
+    for subset in subsets:
+        print(subset)
