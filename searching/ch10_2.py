926b36e6b1a978e964a32a61c0343703bc1a03cb
Kittonn <tonkittipod@gmail.com>
2023-09-30 21:02:31 +0700

    feat(searching): update chapter 10 item 2

--- searching/ch10_2.py
+++ searching/ch10_2.py
@@ -1,32 +1,16 @@
-def insertionSort(arr):
-    n = len(arr)
-    for i in range(1, n):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and key < arr[j]:
-            arr[j + 1] = arr[j]
-            j -= 1
-
-        arr[j + 1] = key
-
 def searching(target, arr):
     for i in range(len(arr)):
         if arr[i] > target:
-            return i
-    return None
+            return arr[i]
+    return "No First Greater Value"
+
 
 if __name__ == "__main__":
     inp, targets = input("Enter Input : ").split("/")
     inp = list(map(int, inp.split()))
     targets = list(map(int, targets.split()))
 
-    insertionSort(inp)
+    inp = sorted(inp)
 
     for target in targets:
-        idx =  searching(target, inp)
-        if idx is not None:
-            print(inp[idx])
-        else:
-            print('No First Greater Value')
-
-        
+        print(searching(target, inp))
