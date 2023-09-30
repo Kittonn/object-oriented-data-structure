1612a122499b71bb24faf443983b6e449711c6cf
Kittonn <tonkittipod@gmail.com>
2023-09-30 17:44:12 +0700

    feat(sorting): update chapter 9 item 4

--- sorting/ch9_4.py
+++ sorting/ch9_4.py
@@ -1,6 +1,15 @@
-def bubble_sort(arr):
-  pass
+def find_alphabet_chars(word):
+    return [char for char in word if char.islower()]
 
 if __name__ == '__main__':
-  inp = input("Enter Input : ").split()
-  print(inp)
+    word_list = input("Enter Input : ").split()
+
+    for i in range(len(word_list)):
+        for j in range(len(word_list) - i - 1):
+            alpha1 = find_alphabet_chars(word_list[j])
+            alpha2 = find_alphabet_chars(word_list[j + 1])
+
+            if alpha1 > alpha2:
+                word_list[j], word_list[j + 1] = word_list[j + 1], word_list[j]
+
+    print(*word_list)
