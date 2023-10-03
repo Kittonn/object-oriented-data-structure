e8b1b8338f2ac9e7bcf9d8adb0ec04bb6536aa43
Kittonn <tonkittipod@gmail.com>
2023-10-04 04:17:33 +0700

    feat(searching): add chapter 10 item 1

--- searching/ch10_1.py
+++ searching/ch10_1.py
@@ -1,18 +1,36 @@
-def search_row_min(inp):
-    min = inp[0]
-    for i in range(1, len(inp)):
-        if inp[i] < min:
-            min = inp[i]
-    return inp.index(min) // table[0]
+def find_min_index(arr):
+    min_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < arr[min_index]:
+            min_index = i
+    return min_index
+
+
+def find_max_index_in_row(min_index):
+    max_index = 0
+    for i in range(col):
+        idx = row * min_index + i
+        if idx < len(arr) and arr[idx] > arr[max_index]:
+            max_index = idx
+    return max_index
+
+
+def find_max_index_in_col(max_idx):
+    max_index = 0
+    for i in range(row):
+        idx = row * i + max_idx
+        if idx < len(arr) and arr[idx] > arr[max_index]:
+            max_index = idx
+    return max_index
 
 
 if __name__ == "__main__":
     table, inp = input("input : ").split(",")
     table = list(map(int, table.split()))
-    inp = list(map(int, inp.split()))
+    arr = list(map(int, inp.split()))
+    row, col = table[0], table[1]
 
-    search = search_row_min(inp)
-
-    find_col = search % table[0]
-
-    print(search)
+    min_idx = find_min_index(arr) // col
+    row_max_idx = find_max_index_in_row(min_idx)
+    col_max_idx = find_max_index_in_col(row_max_idx % row)
+    print(arr[col_max_idx])
